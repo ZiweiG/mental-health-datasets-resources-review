30ac73a544734491562c56dcf55c43bf9954f8a5
Thai On <onquocthai1106@gmail.com>
2025-09-24 22:48:55 -0400

    initial set up testing

---
+++ mental-health-datasets-resources-review/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello, this is Mental Health Data Page
+      </header>
+    </div>
+  );
+}
+
+export default App;
